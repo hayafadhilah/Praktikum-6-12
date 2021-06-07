75fc2705c55e33517479ce3b833b15a509edad63
hayafadhilah <72660457+hayafadhilah@users.noreply.github.com>
2021-06-08 00:16:36 +0700

    Add files via upload

---
+++ Praktikum 10 bag 1.cpp
@@ -0,0 +1,25 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=0;i<20;i++){
+cout<<A[i]<<" ";
+}
+printf("HAYA FADHILAH\n");
+ printf("20051397005\n");
+ printf("MI_A_2020\n");
+cout<<"\nData yang ingin dicari : ";
+cin>>c;
+i=0;
+posisi=0;
+while(i<19 && A[i]!=c){
+ i++;
+}
+if (A[i]!=c){
+ cout<<"Maaf data yang dicari tidak ada";
+}else if(posisi=i+1)
+   cout<<"ditemukan pada posisi ke "<<posisi;
+getch();
+}
+© 2021 GitHub, Inc.
---
+++ Praktikum 6 bag 3.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+void array(int [],int,int);
+int main(){
+ int a[]={0,1,2,3,4,5,6,7,8};
+ int x, i=8;
+ printf("HAYA FADHILAH\n");
+ printf("20051397005\n");
+ printf("MI_A_2020\n");
+ printf("Input x : ");scanf("%d", &x);
+ array(a,x,i);
+}
+void array(int a[],int x,int i){
+ if(i<0){
+ puts("x bukan merupakan anggota array a");
+ return;
+ }
+ if(x==a[i]){
+ puts("x merupakan anggota array a");
+ return;
+ }
+ array(a,x,i-1);
+}
